127458a01d5f4ded710a43317d7e0f89978e1c8f
kurisu <kurisulove@duck.com>
2023-01-09 13:48:51 +0800

    add todo

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 random image api written by go.
 
 **all images should be placed at images directory.**
+
+## todo
+- [ ] cors
+- [ ] better logger 
--- routers/app.go
+++ routers/app.go
@@ -1,23 +1,15 @@
 package routers
 
 import (
-	"net/http"
 	"picview/services"
 
 	"github.com/gin-gonic/gin"
 )
 
 func Routers() (router *gin.Engine) {
-
 	router = gin.Default()
 
 	rgroup := router.Group("/api")
-
-	rgroup.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
 
 	rgroup.GET("/random", services.Random)
 	rgroup.Static("/images", "./images")
